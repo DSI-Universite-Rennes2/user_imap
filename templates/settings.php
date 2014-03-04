2b1ad1e8d365c503ac1ee03898fc52965bc5b4e0
Benedikt Wildenhain <github.com@benedikt-wildenhain.de>
2014-03-04 15:22:54 +0100

    Renamed option 'imap_ssl' into 'imap_ssl_novalidate' to better reflect its meaning

--- templates/settings.php
+++ templates/settings.php
@@ -3,7 +3,7 @@
                <legend><?php echo $l->t('IMAP'); ?></legend>
                <p><label for="imap_host"><?php echo $l->t('Host');?></label><input type="text" id="imap_host" name="imap_host" value="<?php echo $_['imap_host']; ?>"></p>
                <p><label for="imap_port"><?php echo $l->t('Port');?></label><input type="text" id="imap_port" name="imap_port" value="<?php echo $_['imap_port']; ?>" /></p>
-               <p><input type="checkbox" id="imap_ssl" name="imap_ssl" value="1"<?php if ($_['imap_ssl']) echo ' checked'; ?>><label for="imap_ssl"><?php echo $l->t('Don\'t validate SSL certificate');?></label></p>
+               <p><input type="checkbox" id="imap_ssl_novalidate" name="imap_ssl_novalidate" value="1"<?php if ($_['imap_ssl_novalidate']) echo ' checked'; ?>><label for="imap_ssl_novalidate"><?php echo $l->t('Don\'t validate SSL certificate');?></label></p>
        <input type="submit" value="<?php echo $l->t('Save'); ?>" />
        </fieldset>
 </form>
